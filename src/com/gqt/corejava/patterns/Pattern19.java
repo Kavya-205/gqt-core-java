61bf338b97607e913ccd5517283162b330b902ce
kavya <kavya@kaavi-laptop>
2025-07-27 20:07:06 +0530

    loaded all the pattern programs

--- src/com/gqt/corejava/patterns/Pattern19.java
+++ src/com/gqt/corejava/patterns/Pattern19.java
@@ -9,7 +9,7 @@ public class Pattern19 {
 		int k=1;
 		for(int i=n;i>=1;i--) {
 			for(int j=1;j<=i;j++) {
-				System.out.print(k +"  ");
+				System.out.print(k +"   ");
 				k++;
 			}
 			System.out.println();
